308d256e62c9004f0f169ea2f2ed4146121feb71
sophia-ve <sve@teamiken.de>
2021-03-09 13:44:08 +0100

    module

---
+++ src/Module/HelloWorldModule.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sophiave\ContaoHelloWorldBundle\Module;
+
+class HelloWorldModule extends \Module
+{
+    /**
+     * @var string
+     */
+    protected $strTemplate = 'mod_helloWorld';
+
+    /**
+     * Displays a wildcard in the back end.
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        if (TL_MODE == 'BE') {
+            $template = new \BackendTemplate('be_wildcard');
+
+            $template->wildcard = '### '.utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['helloWorld'][0]).' ###';
+            $template->title = $this->headline;
+            $template->id = $this->id;
+            $template->link = $this->name;
+            $template->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id='.$this->id;
+
+            return $template->parse();
+        }
+
+        return parent::generate();
+    }
+
+    /**
+     * Generates the module.
+     */
+    protected function compile()
+    {
+        $this->Template->message = 'Hello World';
+    }
+}
